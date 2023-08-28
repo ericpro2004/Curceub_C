57e5abd539c17742d3428037b302fc3c7e59b9a2
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-08-28 19:06:10 -0300

    Update Cursinho1.c

--- Cursinho1.c
+++ Cursinho1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
 
+float real;
+
+
+
+void main()
+{
+    setlocale(LC_ALL, "");
+    printf("Qual é a quantidade em dinheiro (em reais) a ser convertida para euro e dollar?: \n");
+    scanf("%f", &real);
+    float dollar = real*5;
+    float euro = real*5.2;
+    printf("Você tem %f dolares ou %f euros \n\n", dollar, euro);
+}
