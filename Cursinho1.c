aac3a417bdb814e6c1827f77a35ba7835b103322
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-08-29 19:03:27 -0300

    Update Cursinho1.c

--- Cursinho1.c
+++ Cursinho1.c
@@ -14,3 +14,22 @@ void main()
     float euro = real/5.2;
     printf("Você tem %.2fR$ dolares ou %.2fR$ euros. \n\n", dollar, euro);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int X;
+    loop:
+    printf("Qual é o valor de X?: \n");
+    scanf("%d", &X);
+    int divi = %X;
+    if (X < 101) {
+    printf("Números divisíveis: %d\n", divi);
+    X++;
+    goto loop;
+    }
+}
