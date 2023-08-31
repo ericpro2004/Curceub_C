51be0c6f5304afe016fcd81feb8f5e2252187d9b
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-08-31 17:08:00 -0300

    Update Cursinho1.c

--- Cursinho1.c
+++ Cursinho1.c
@@ -3,33 +3,72 @@
 #include <math.h>
 #include <locale.h>
 
-float real;
 
-void main()
-{
-    setlocale(LC_ALL, "");
-    printf("Qual é a quantidade em dinheiro (em reais) a ser convertida para euro e dollar?: \n");
-    scanf("%f", &real);
-    float dollar = real/5;
-    float euro = real/5.2;
-    printf("Você tem %.2fR$ dolares ou %.2fR$ euros. \n\n", dollar, euro);
+void somar(int num1, int num2, int resultado) {
+    resultado = num1 + num2;
+    printf("O resultado da soma é %d", resultado);
+
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+void subtrair(int num1, int num2, int resultado) {
+    resultado = num1 - num2;
+    printf("O resultado da subtração é %d", resultado);
 
-void main()
-{
+}
+
+void multiplicar(int num1, int num2, int resultado) {
+    resultado = num1 * num2;
+    printf("O resultado da multiplicação é %d", resultado);
+
+}
+
+void dividir(int num1, int num2, int resultado) {
+    resultado = num1 / num2;
+    printf("O resultado da divisão é %d", resultado);
+
+}
+
+int main() {
     setlocale(LC_ALL, "Portuguese");
-    int X;
-    loop:
-    printf("Qual é o valor de X?: \n");
-    scanf("%d", &X);
-    int divi = %X;
-    if (X < 101) {
-    printf("Números divisíveis: %d\n", divi);
-    X++;
-    goto loop;
+    int num1, num2, resultado, tipoop, op;
+    printf("Qual operação deseja utilizar?\n");
+    printf("1 - soma, 2 - subtração, 3 - multiplicação, 4 - divisão: ");
+    scanf("%d", &tipoop);
+    switch(tipoop) {
+        case 1:
+        printf("Digite um número inteiro (positivo ou negativo) +: ");
+        scanf("%d", &num1);
+        printf("Digite outro número inteiro (positivo ou negativo) +: ");
+        scanf("%d", &num2);
+        op = ;
+        printf("O resultado da op é %d", resultado);
+        break;
+
+        case 2:
+        printf("Digite um número inteiro (positivo ou negativo) -: ");
+        scanf("%d", &num1);
+        printf("Digite outro número inteiro (positivo ou negativo) -: ");
+        scanf("%d", &num2);
+        printf(resultado);
+        break;
+
+        case 3:
+        printf("Digite um número inteiro (positivo ou negativo) *: ");
+        scanf("%d", &num1);
+        printf("Digite outro número inteiro (positivo ou negativo) *: ");
+        scanf("%d", &num2);
+        printf("O resultado da op é %d", resultado);
+        break;
+
+        case 4:
+        printf("Digite um número inteiro (positivo ou negativo) /: ");
+        scanf("%d", &num1);
+        printf("Digite outro número inteiro (positivo ou negativo) /: ");
+        scanf("%d", &num2);
+        printf(resultado);
+        break;
+
+        default:
+            printf("Inválido!");
     }
 }
