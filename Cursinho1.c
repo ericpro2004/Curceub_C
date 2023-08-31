e1bf20e786ae25e8dc450b117ca48937f182fee1
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-08-31 17:14:51 -0300

    Update Cursinho1.c

--- Cursinho1.c
+++ Cursinho1.c
@@ -5,67 +5,49 @@
 
 
 void somar(int num1, int num2, int resultado) {
-    resultado = num1 + num2;
-    printf("O resultado da soma é %d", resultado);
+    return num1 + num2;
 
 }
 
 void subtrair(int num1, int num2, int resultado) {
-    resultado = num1 - num2;
-    printf("O resultado da subtração é %d", resultado);
+    return num1 - num2;
 
 }
 
 void multiplicar(int num1, int num2, int resultado) {
-    resultado = num1 * num2;
-    printf("O resultado da multiplicação é %d", resultado);
+    return num1 * num2;
 
 }
 
 void dividir(int num1, int num2, int resultado) {
-    resultado = num1 / num2;
-    printf("O resultado da divisão é %d", resultado);
+    return num1 / num2;
 
 }
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
     int num1, num2, resultado, tipoop, op;
+    printf("Digite um número inteiro (positivo ou negativo) +: ");
+    scanf("%d", &num1);
+    printf("Digite outro número inteiro (positivo ou negativo) +: ");
+    scanf("%d", &num2);
     printf("Qual operação deseja utilizar?\n");
     printf("1 - soma, 2 - subtração, 3 - multiplicação, 4 - divisão: ");
     scanf("%d", &tipoop);
     switch(tipoop) {
         case 1:
-        printf("Digite um número inteiro (positivo ou negativo) +: ");
-        scanf("%d", &num1);
-        printf("Digite outro número inteiro (positivo ou negativo) +: ");
-        scanf("%d", &num2);
-        op = ;
-        printf("O resultado da op é %d", resultado);
+            
         break;
 
         case 2:
-        printf("Digite um número inteiro (positivo ou negativo) -: ");
-        scanf("%d", &num1);
-        printf("Digite outro número inteiro (positivo ou negativo) -: ");
-        scanf("%d", &num2);
-        printf(resultado);
+
         break;
 
         case 3:
-        printf("Digite um número inteiro (positivo ou negativo) *: ");
-        scanf("%d", &num1);
-        printf("Digite outro número inteiro (positivo ou negativo) *: ");
-        scanf("%d", &num2);
-        printf("O resultado da op é %d", resultado);
         break;
 
         case 4:
-        printf("Digite um número inteiro (positivo ou negativo) /: ");
-        scanf("%d", &num1);
-        printf("Digite outro número inteiro (positivo ou negativo) /: ");
-        scanf("%d", &num2);
-        printf(resultado);
+
         break;
 
         default:
